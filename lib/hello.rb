00d6902bacb0fa14877d984a35ceae0aa3f7deaa
sensible-partial-6157 <daniyellaharmon@gmail.com>
2020-09-21 17:12:44 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -6,11 +6,6 @@ while i < array.length
   i = i + 1
 end
 
-# call your method here!
-["Tim", "Tom", "Jim"]
-hello_t(["Tim", "Tom", "Jim"]) do |name|
-  if name.start_with?("T")
-    puts "Hi, #{name}"
-  end
+  array
 end
 
